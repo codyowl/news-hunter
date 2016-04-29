011ad2fbec53ed13ebe1e2f85b9f7195a7397e9f
codyowl <codyowl@gmail.com>
2016-04-29 10:49:35 +0530

    Got The Hindu's Headlines

--- news_hunter.py
+++ news_hunter.py
@@ -7,11 +7,25 @@ Which one you want to read ?
 2.INDIAN EXPRESS
 3.DECCAN CHRONICLE
 4.THE TIMES OF INDIA
-"""
+""")
 
 if Newspaper == '1':
 	newspaper_url = "http://www.thehindu.com/"
-	
+	requester = urllib2.Request(newspaper_url, headers={'User-Agent': "Magic Browser"})
+	connector = urllib2.urlopen(requester)
+	connector_reader = connector.read()
+	soup = BeautifulSoup(connector_reader, "lxml")
+
+	Headlines_div = soup.findAll("div", {"class" : "h-main-lead posRel"})
+
+	HEADLINES_LIST = []
+
+	for div in Headlines_div:
+		content = div.find('h1').find('a').contents[0]
+		HEADLINES_LIST.append(str(content))
+
+	print HEADLINES_LIST	
+
 elif Newspaper == '2':
 	newspaper_url = "http://indianexpress.com/"
 	
@@ -25,7 +39,10 @@ else:
 	print "Make suer you've entered the right number"
 
 
-def url_opener(url):
-	requester = urllib2.Request(url, headers={'User-Agent': "Magic Browser"}
-	connector = urllib2.urlopen(requester)
-	connector_reader = connector.read()                       
+# def url_opener(url):
+# 	requester = urllib2.Request(url, headers={'User-Agent': "Magic Browser"}
+# 	connector = urllib2.urlopen(requester)
+# 	connector_reader = connector.read()
+# 	return connector_reader
+
+                       
